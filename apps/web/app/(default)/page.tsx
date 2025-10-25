2e14479e9b536474a3cb2979e3072ea8c17cf25f
Reza SoltaniTehrani <r.soltani2000@gmail.com>
2025-10-25 20:19:34 +0330

    fix charts and panel items

--- apps/web/app/(default)/page.tsx
+++ apps/web/app/(default)/page.tsx
@@ -1,6 +1,17 @@
+import { ChartAreaStacked } from "./components/ArearChart";
+import { ChartLineMultiple } from "./components/LineChart";
+import { ChartPieLabel } from "./components/PieChart";
+import { ChartRadarDots } from "./components/RadarChart";
 
 export default function Home() {
-    return (
-        <div>kasjdlasdjk</div>
-    )
+  return (
+    <div className="mt-16">
+      <ChartAreaStacked />
+      <div className="flex items-start gap-8 mt-16">
+        <div className="w-full"><ChartPieLabel /></div>
+        <div className="w-full"><ChartLineMultiple /></div>
+        <div className="w-full"><ChartRadarDots /></div>
+      </div>
+    </div>
+  );
 }
