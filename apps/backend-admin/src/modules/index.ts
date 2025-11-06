7d6f652cc8330c65bc1c415ba6542a4a6a546948
Reza SoltaniTehrani <r.soltani2000@gmail.com>
2025-11-06 10:39:32 +0330

    create provinces route

--- apps/backend-admin/src/modules/index.ts
+++ apps/backend-admin/src/modules/index.ts
@@ -1,8 +1,10 @@
 import { FastifyInstance } from 'fastify'
 import { countriesModule } from './countries'
 import { usersModule } from './users'
+import { provincesModule } from './provinces'
 
 export async function initialModules(app: FastifyInstance) {
   await countriesModule(app)
   await usersModule(app)
+  await provincesModule(app)
 }
--- apps/backend-admin/src/plugins/prisma.ts
+++ apps/backend-admin/src/plugins/prisma.ts
@@ -1,8 +1,8 @@
 // src/plugins/prisma.ts
+import { PrismaClient } from "@prisma/client";
 import { FastifyInstance } from "fastify";
 import fp from "fastify-plugin";
-import { PrismaClient } from "@nikrad/database";
-
+ 
 export async function setup(app: FastifyInstance) {
   const prismaPlugin = fp(async (app: FastifyInstance) => {
     const prisma = new PrismaClient();
