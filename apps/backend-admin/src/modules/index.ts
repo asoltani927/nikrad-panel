4f4a63e189ce425331dd5804a202a3b4393a1f68
Reza SoltaniTehrani <r.soltani2000@gmail.com>
2025-11-08 18:22:22 +0330

    create crud category api

--- apps/backend-admin/src/modules/index.ts
+++ apps/backend-admin/src/modules/index.ts
@@ -3,10 +3,12 @@ import { countriesModule } from './countries'
 import { usersModule } from './users'
 import { provincesModule } from './provinces'
 import { needsModule } from './needs'
+import { categoriesModule } from './categories'
 
 export async function initialModules(app: FastifyInstance) {
   await countriesModule(app)
   await usersModule(app)
   await provincesModule(app)
   await needsModule(app)
+  await categoriesModule(app)
 }
