d90f9c3abbbf7f34c909d8d28266052867f4461e
Amin Soltani <asoltani927@gmail.com>
2025-11-24 18:34:58 +0330

    Revert "chore:update"
    This reverts commit b3f0ffcafc2e9919f83372c8157d01ae926020d8.

--- apps/backend-admin/src/modules/custom-fields/routes/put-custom-field.route.ts
+++ apps/backend-admin/src/modules/custom-fields/routes/put-custom-field.route.ts
@@ -12,7 +12,7 @@ const UpdateCustomFieldParamsSchema = z.object({
 const UpdateCustomFieldBodySchema = z.object({
   name: z.string().min(1).optional(),
   title: z.string().min(1).optional(),
-  type: z.enum(Object.values(CustomFieldType) as [string, ...string[]]),
+  type: z.nativeEnum(CustomFieldType).optional(),
   required: z.boolean().optional(),
   order: z.number().nullable().optional(),
   step: z.number().nullable().optional(),
