a06940a931b4b05a1e03fdaada3fe9d760b2e5e9
Amin Soltani <asoltani927@gmail.com>
2025-12-02 13:40:10 +0330

    chore chore: update repo names

--- apps/backend-admin/entrypoint.sh
+++ apps/backend-admin/entrypoint.sh
@@ -5,8 +5,8 @@ echo "🚀 Starting entrypoint script..."
 
 # Run Prisma migrations for the API package
 echo "🏗️  Running Prisma migrations..."
-pnpm --filter @repo/api migrate:deploy
+pnpm --filter @nikrad/api migrate:deploy
 
 # Start the Fastify API server
 echo "✅ Starting Fastify server..."
-pnpm --filter @repo/api start
+pnpm --filter @nikrad/api start
