bbc704e59f38ab0b53fe8ba64b1ca568ec72808d
Reza SoltaniTehrani <r.soltani2000@gmail.com>
2026-01-04 17:10:59 +0330

    create products crud api

--- apps/backend-client/src/modules/products/index.ts
+++ apps/backend-client/src/modules/products/index.ts
@@ -4,6 +4,7 @@ import { getProductsRoute } from './routes/get-products.route'
 import { getProductByIdRoute } from './routes/get-product.route'
 import { getProductBySellerIdRoute } from './routes/get-products-seller.route'
 import { putProductRoute } from './routes/put-product.route'
+import { deleteProductRoute } from './routes/delete-product.route'
 
 export async function productsModule(app: FastifyInstance) {
   app.register(
@@ -13,6 +14,7 @@ export async function productsModule(app: FastifyInstance) {
       productsApp.register(getProductByIdRoute)
       productsApp.register(getProductBySellerIdRoute)
       productsApp.register(putProductRoute)
+      productsApp.register(deleteProductRoute)
     },
     { prefix: '/products' },
   )
