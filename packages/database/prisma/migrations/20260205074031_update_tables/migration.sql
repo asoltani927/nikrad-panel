b1707e299537dd43026d7fec2fa0370297dc635b
Amin Soltani <asoltani927@gmail.com>
2026-02-05 11:11:27 +0330

    fix(database): fix migrations

---
+++ packages/database/prisma/migrations/20260205074031_update_tables/migration.sql
@@ -0,0 +1,231 @@
+*/
+-- CreateEnum
+CREATE TYPE "OptionsType" AS ENUM ('REGION', 'CUSTOM');
+
+-- CreateEnum
+CREATE TYPE "PaymentGateway" AS ENUM ('ZARINPAL', 'ZIBAL', 'PAYIR', 'MELLAT');
+
+-- CreateEnum
+CREATE TYPE "TransactionStatus" AS ENUM ('PENDING', 'SUCCESS', 'FAILED');
+
+-- CreateEnum
+CREATE TYPE "OtpType" AS ENUM ('login', 'verify', 'reset');
+
+-- DropForeignKey
+ALTER TABLE "material_book" DROP CONSTRAINT "material_book_ownerId_fkey";
+
+-- AlterTable
+ALTER TABLE "Address" DROP COLUMN "createdAt",
+DROP COLUMN "deletedAt",
+DROP COLUMN "updatedAt",
+ADD COLUMN     "created_at" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+ADD COLUMN     "deleted_at" TIMESTAMP(3),
+ADD COLUMN     "updated_at" TIMESTAMP(3) NOT NULL;
+
+-- AlterTable
+ALTER TABLE "File" DROP COLUMN "createdAt",
+DROP COLUMN "deletedAt",
+DROP COLUMN "updatedAt",
+ADD COLUMN     "created_at" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+ADD COLUMN     "deleted_at" TIMESTAMP(3),
+ADD COLUMN     "updated_at" TIMESTAMP(3) NOT NULL;
+
+-- AlterTable
+ALTER TABLE "MaterialBookAttachment" DROP COLUMN "createdAt",
+DROP COLUMN "deletedAt",
+DROP COLUMN "updatedAt",
+ADD COLUMN     "created_at" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+ADD COLUMN     "deleted_at" TIMESTAMP(3),
+ADD COLUMN     "updated_at" TIMESTAMP(3) NOT NULL;
+
+-- AlterTable
+ALTER TABLE "User" DROP COLUMN "createdAt",
+DROP COLUMN "deletedAt",
+DROP COLUMN "updatedAt",
+ADD COLUMN     "created_at" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+ADD COLUMN     "cuid" TEXT NOT NULL,
+ADD COLUMN     "deleted_at" TIMESTAMP(3),
+ADD COLUMN     "updated_at" TIMESTAMP(3) NOT NULL;
+
+-- AlterTable
+ALTER TABLE "categories" DROP COLUMN "createdAt",
+DROP COLUMN "deletedAt",
+DROP COLUMN "updatedAt",
+ADD COLUMN     "created_at" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+ADD COLUMN     "deleted_at" TIMESTAMP(3),
+ADD COLUMN     "updated_at" TIMESTAMP(3) NOT NULL;
+
+-- AlterTable
+ALTER TABLE "custom_field_values" DROP COLUMN "createdAt",
+DROP COLUMN "updatedAt",
+ADD COLUMN     "created_at" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+ADD COLUMN     "updated_at" TIMESTAMP(3) NOT NULL;
+
+-- AlterTable
+ALTER TABLE "custom_fields" DROP COLUMN "createdAt",
+DROP COLUMN "deletedAt",
+DROP COLUMN "updatedAt",
+ADD COLUMN     "created_at" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+ADD COLUMN     "deleted_at" TIMESTAMP(3),
+ADD COLUMN     "options_type" "OptionsType" NOT NULL DEFAULT 'CUSTOM',
+ADD COLUMN     "updated_at" TIMESTAMP(3) NOT NULL;
+
+-- AlterTable
+ALTER TABLE "material_book" DROP COLUMN "createdAt",
+DROP COLUMN "deletedAt",
+DROP COLUMN "ownerId",
+DROP COLUMN "title",
+DROP COLUMN "updatedAt",
+ADD COLUMN     "created_at" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+ADD COLUMN     "cuid" TEXT NOT NULL,
+ADD COLUMN     "deleted_at" TIMESTAMP(3),
+ADD COLUMN     "owner_id" INTEGER NOT NULL,
+ADD COLUMN     "updated_at" TIMESTAMP(3) NOT NULL;
+
+-- AlterTable
+ALTER TABLE "material_book_package_info" DROP COLUMN "createdAt",
+DROP COLUMN "updatedAt",
+ADD COLUMN     "created_at" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+ADD COLUMN     "updated_at" TIMESTAMP(3) NOT NULL;
+
+-- AlterTable
+ALTER TABLE "needs" DROP COLUMN "createdAt",
+DROP COLUMN "deletedAt",
+DROP COLUMN "updatedAt",
+ADD COLUMN     "created_at" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+ADD COLUMN     "deleted_at" TIMESTAMP(3),
+ADD COLUMN     "updated_at" TIMESTAMP(3) NOT NULL;
+
+-- AlterTable
+ALTER TABLE "suggestions" DROP COLUMN "createdAt",
+DROP COLUMN "deletedAt",
+DROP COLUMN "updatedAt",
+ADD COLUMN     "created_at" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+ADD COLUMN     "deleted_at" TIMESTAMP(3),
+ADD COLUMN     "status" TEXT NOT NULL DEFAULT 'draft',
+ADD COLUMN     "updated_at" TIMESTAMP(3) NOT NULL;
+
+-- CreateTable
+CREATE TABLE "Transaction" (
+    "id" SERIAL NOT NULL,
+    "amount" INTEGER NOT NULL,
+    "authority" TEXT,
+    "refId" TEXT,
+    "status" "TransactionStatus" NOT NULL DEFAULT 'PENDING',
+    "gateway" "PaymentGateway" NOT NULL,
+    "userId" INTEGER,
+    "orderId" INTEGER,
+    "created_at" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "paidAt" TIMESTAMP(3),
+
+    CONSTRAINT "Transaction_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "Shop" (
+    "id" SERIAL NOT NULL,
+    "cuid" TEXT NOT NULL,
+    "name" TEXT NOT NULL,
+    "about" TEXT NOT NULL,
+    "about_seller" TEXT NOT NULL,
+    "days_of_activity" TEXT[],
+    "working_hours" JSONB NOT NULL,
+    "response_hours" JSONB NOT NULL,
+    "social_media" JSONB,
+    "success_deals" INTEGER NOT NULL DEFAULT 0,
+    "failed_deals" INTEGER NOT NULL DEFAULT 0,
+    "thumbnail_image" TEXT,
+    "owner_id" INTEGER,
+    "category_id" INTEGER,
+    "deleted" BOOLEAN NOT NULL DEFAULT false,
+    "deleted_at" TIMESTAMP(3),
+    "created_at" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updated_at" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "Shop_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "ShopGallery" (
+    "id" SERIAL NOT NULL,
+    "image_url" TEXT NOT NULL,
+    "shopId" INTEGER NOT NULL,
+
+    CONSTRAINT "ShopGallery_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "ShopReview" (
+    "id" SERIAL NOT NULL,
+    "rating" INTEGER NOT NULL,
+    "comment" TEXT,
+    "user_id" INTEGER NOT NULL,
+    "shop_id" INTEGER NOT NULL,
+    "created_at" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+
+    CONSTRAINT "ShopReview_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "otps" (
+    "id" TEXT NOT NULL,
+    "phone" TEXT NOT NULL,
+    "type" "OtpType" NOT NULL,
+    "code" TEXT NOT NULL,
+    "expiresAt" TIMESTAMP(3) NOT NULL,
+    "usedAt" TIMESTAMP(3),
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+
+    CONSTRAINT "otps_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateIndex
+CREATE INDEX "Transaction_authority_idx" ON "Transaction"("authority");
+
+-- CreateIndex
+CREATE INDEX "Transaction_userId_idx" ON "Transaction"("userId");
+
+-- CreateIndex
+CREATE INDEX "Transaction_orderId_idx" ON "Transaction"("orderId");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "Shop_cuid_key" ON "Shop"("cuid");
+
+-- CreateIndex
+CREATE INDEX "otps_phone_idx" ON "otps"("phone");
+
+-- CreateIndex
+CREATE INDEX "otps_expiresAt_idx" ON "otps"("expiresAt");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "otps_phone_type_code_usedAt_key" ON "otps"("phone", "type", "code", "usedAt");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "User_cuid_key" ON "User"("cuid");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "custom_field_values_customFieldId_target_id_key" ON "custom_field_values"("customFieldId", "target_id");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "material_book_cuid_key" ON "material_book"("cuid");
+
+-- AddForeignKey
+ALTER TABLE "material_book" ADD CONSTRAINT "material_book_owner_id_fkey" FOREIGN KEY ("owner_id") REFERENCES "User"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "Transaction" ADD CONSTRAINT "Transaction_userId_fkey" FOREIGN KEY ("userId") REFERENCES "User"("id") ON DELETE SET NULL ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "Shop" ADD CONSTRAINT "Shop_owner_id_fkey" FOREIGN KEY ("owner_id") REFERENCES "User"("id") ON DELETE SET NULL ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "Shop" ADD CONSTRAINT "Shop_category_id_fkey" FOREIGN KEY ("category_id") REFERENCES "categories"("id") ON DELETE SET NULL ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "ShopGallery" ADD CONSTRAINT "ShopGallery_shopId_fkey" FOREIGN KEY ("shopId") REFERENCES "Shop"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "ShopReview" ADD CONSTRAINT "ShopReview_user_id_fkey" FOREIGN KEY ("user_id") REFERENCES "User"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "ShopReview" ADD CONSTRAINT "ShopReview_shop_id_fkey" FOREIGN KEY ("shop_id") REFERENCES "Shop"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
